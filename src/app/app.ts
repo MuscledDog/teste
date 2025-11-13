8492052998ac82faa8540a75539bec4b79e8171a
MuscledDog <lemos.vasco@gmail.com>
2025-11-13 22:58:59 +0000

    2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Footer } from "./componentes/footer/footer";
+import { Main } from "./componentes/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
